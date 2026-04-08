65be510128cd76759f0b08e51ff2e30f941e4205
zyyx-thuanlv <thuanlv@zyyx.jp>
2026-04-08 19:53:36 +0700

    add folder db

---
+++ db/ddl.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `history` (
+  `id` int NOT NULL,
+  `busted` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hash` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+
+CREATE TABLE `case_3` (
+  `id` int NOT NULL,
+  `count` smallint DEFAULT NULL,
+  `busted` int NOT NULL,
+  `dead_flg` tinyint DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+
+CREATE TABLE `case_5` (
+  `id` int NOT NULL,
+  `count` smallint DEFAULT NULL,
+  `busted` int NOT NULL,
+  `dead_flg` tinyint DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+
+CREATE TABLE `case_7` (
+  `id` int NOT NULL,
+  `count` smallint DEFAULT NULL,
+  `busted` int NOT NULL,
+  `dead_flg` tinyint DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+
+CREATE TABLE `case_10` (
+  `id` int NOT NULL,
+  `count` smallint DEFAULT NULL,
+  `busted` int NOT NULL,
+  `dead_flg` tinyint DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
